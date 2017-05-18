5e4f6f038f0167f794b2c7f941bd2a563f3ccdde
Karim Hernandez <karimhernandez@gmail.com>
2017-05-18 16:57:42 -0700

    Updated namer to default to a new color search function, pluckNearest.  Expose old function as getResults.  Minor format revisions.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 "use strict";
 
-var distance = require('euclidean-distance')
-var chroma = require('chroma-js')
-var color
-var key
+var distance = require('euclidean-distance');
+var chroma = require('chroma-js');
+var color;
+var key;
 
 // These `require` statements are all explicit
 // to keep the browserify build from breaking
@@ -14,23 +14,48 @@ var lists = {
   pantone: require('./lib/colors/pantone'),
   roygbiv: require('./lib/colors/roygbiv'),
   x11: require('./lib/colors/x11')
-}
+};
 
-var namer = module.exports = function(color) {
-  color = chroma(color)
-  var results = {}
+var getResults = function(color) {
+  color = chroma(color);
+  var results = {};
   for (key in lists) {
     results[key] = lists[key]
       .map (function(name) {
-        name.distance = distance(color.lab(), chroma(name.hex).lab())
-        return name
+        name.distance = distance(color.lab(), chroma(name.hex).lab());
+        return name;
       })
       .sort (function(a, b) {
-        return a.distance - b.distance
+        return a.distance - b.distance;
       })
   }
-  return results
-}
+  return results;
+};
 
-namer.chroma = chroma
-namer.lists = lists
+var pluckNearest = function(color) {
+  var i, len, keys, match, result, results, shortest = 30;
+
+	//color dictionaries in order of preference.
+  // TODO: expose list as option.
+	keys = ['basic', 'html', 'pantone', 'ntc', 'x11']; // 'roygbiv'
+  color = chroma(color);
+  results = getResults(color);
+
+	for (i = 0, len = keys.length; i < len; i++) {
+		match = results[keys[i]][0];
+		if (shortest > match.distance) {
+			shortest = match.distance;
+			result = match;
+			result.list = keys[i];
+		}
+	}
+
+	return result;
+};
+
+var namer = pluckNearest;
+namer.getResults = getResults;
+namer.chroma = chroma;
+namer.lists = lists;
+
+module.exports = namer;
