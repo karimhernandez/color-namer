37dc432600cd53ceb9c7948145d3d77024ba49c7
Karim Hernandez <karimhernandez@gmail.com>
2017-05-19 11:52:09 -0700

    Updated namer to accept optional configuration.  Ability to specify which lists to use for nearest().  Ability to supply a custom colors array.

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 
 var distance = require('euclidean-distance');
 var chroma = require('chroma-js');
-var color;
-var key;
+var options;
 
 // These `require` statements are all explicit
 // to keep the browserify build from breaking
@@ -16,45 +15,71 @@ var lists = {
   x11: require('./lib/colors/x11')
 };
 
-var getResults = function(color) {
-  color = chroma(color);
+var options = {
+  keys: ['basic', 'html', 'ntc', 'pantone', 'roygbiv', 'x11'],
+  colors: [] // Color lists expected to have { name: 'foo', hex: '#FFF' }
+};
+
+function opt(options, name, defaultValue) {
+  return options && options[name] !== undefined ? options[name] : defaultValue;
+}
+
+function getResults(color) {
   var results = {};
-  for (key in lists) {
+  if (options.colors.length > 0) {
+    lists.custom = options.colors;
+  }
+  for (var key in lists) {
     results[key] = lists[key]
-      .map (function(name) {
+      .map(function (name) {
         name.distance = distance(color.lab(), chroma(name.hex).lab());
         return name;
       })
-      .sort (function(a, b) {
+      .sort(function (a, b) {
         return a.distance - b.distance;
       })
   }
   return results;
-};
+}
 
-var pluckNearest = function(color) {
+function pluckNearest(color) {
   var i, len, keys, match, result, results, shortest = 30;
 
-	//color dictionaries in order of preference.
-  // TODO: expose list as option.
-	keys = ['basic', 'html', 'pantone', 'ntc', 'x11']; // 'roygbiv'
-  color = chroma(color);
   results = getResults(color);
+  keys = options.keys;
 
-	for (i = 0, len = keys.length; i < len; i++) {
-		match = results[keys[i]][0];
-		if (shortest > match.distance) {
-			shortest = match.distance;
-			result = match;
-			result.list = keys[i];
-		}
-	}
+  for (i = 0, len = keys.length; i < len; i++) {
+    if (results[keys[i]]) {
+      match = results[keys[i]][0];
+      if (shortest > match.distance) {
+        shortest = match.distance;
+        result = match;
+        result.list = keys[i];
+      }
+    }
+  }
 
-	return result;
-};
+  return result;
+}
 
-var namer = pluckNearest;
-namer.getResults = getResults;
+function main(color, opts) {
+  color = chroma(color);
+
+  opts = opts || {};
+  if (opts) {
+    options.keys = opt(opts, 'lists', options.keys);
+    options.colors = opt(opts, 'colors', options.colors);
+  }
+
+  options.keys.unshift('custom');
+
+  return pluckNearest(color);
+}
+
+
+var namer = main;
+namer.nearest = pluckNearest;
+namer.results = getResults;
 namer.chroma = chroma;
 namer.lists = lists;
 
